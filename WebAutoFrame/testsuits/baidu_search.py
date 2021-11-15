343312ff5bccc240daac1d9e52b51675fa4d8b1f
wsaicyj <wsaicyj@163.com>
2021-11-16 00:14:16 +0800

    frame-6

--- WebAutoFrame/testsuits/baidu_search.py
+++ WebAutoFrame/testsuits/baidu_search.py
@@ -7,21 +7,22 @@ from WebAutoFrame.pageobjects.baidu_homepage import HomePage
 
 class BaiduSearch(unittest.TestCase):
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         '''
         测试固件的setUp()的代码，主要是测试的前提准备工作
         :return:
         '''
-        browser = BrowserEngine(self)
-        self.driver = browser.open_browser(self)
+        browser = BrowserEngine(cls)
+        cls.driver = browser.open_browser(cls)
 
-
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(cls):
         '''
         测试结束后的操作，这里基本都是关闭浏览器
         :return:
         '''
-        self.driver.quit()
+        cls.driver.quit()
 
     def test_baidu_search(self):
         '''
@@ -37,6 +38,13 @@ class BaiduSearch(unittest.TestCase):
             print('Test pass.')
         except Exception as e:
             print('Test Fail.', format(e))
+
+    def test_search2(self):
+        homepage = HomePage(self.driver)
+        homepage.type_search('python')
+        homepage.send_submit_btn()
+        time.sleep(1)
+        homepage.get_windows_img()
 
 if __name__ == '__main__':
     unittest.main()
